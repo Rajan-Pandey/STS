c7d710f06374682183a244610fca090f8a1db5fb
Rajan-Pandey <pandeyrajan960@gmail.com>
2022-09-05 23:45:20 +0530

    Structural Changes
    Separate Pair class created instead of Inner class
    Lamba Expression used for sorting

--- comp_practice/Activity.java
+++ comp_practice/Activity.java
@@ -1,9 +1,7 @@
 import java.util.*;
 
-public class Activity
-{
-
-static class pair implements Comparable<pair>
+class pair 
+//implements Comparable<pair>
 {
 int start;
 int end;
@@ -14,10 +12,35 @@ this.start = start;
 this.end = end;
 }
 
-public int compareTo(pair p1)
+/* public int compareTo(pair p1)
 {
 return this.end-p1.end;
 }
+*/
+}
+
+public class Activity
+{
+public int findActivityCount(int[] start, int[] end, int n)
+{
+int count = 1;
+int j = 0;  // first activity is always selected hence count = 1 and j points to index of those activities that are selected
+pair[] p = new pair[n];
+for(int i=0;i<n;i++)
+	p[i] = new pair(start[i], end[i]);
+Arrays.sort(p,(a,b) ->  a.end - b.end);
+System.out.println("::Possible sequence of activities are ::");
+System.out.println("(" + p[j].start + "," + p[j].end +")"); 
+for(int i=1;i<n;i++)
+{
+	if(p[i].start >= p[j].end)
+	{
+		System.out.println("(" + p[i].start + "," + p[i].end + ")");
+		count++;
+		j = i;
+	}	
+}
+return count;
 }
 public static void main(String[] args)
 {
@@ -32,30 +55,8 @@ for(int i=0;i<n;i++)
 System.out.print("Enter end time of activities : ");
 for(int i=0;i<n;i++)
 	end[i] = sc.nextInt();
-
-System.out.print("Max no. of Activities Possible  are : " + findActivityCount(start,end,n));
+Activity activity = new Activity();
+System.out.println("Max no. of Activities Possible  are : " + activity.findActivityCount(start,end,n));
 }
 
-public static int findActivityCount(int[] start, int[] end, int n)
-{
-int count = 1;
-int j = 0;  // first activity is always selected hence count = 1 and j points to index of those activities that are selected
-pair[] p = new pair[n];
-for(int i=0;i<n;i++)
-	p[i] = new pair(start[i], end[i]);
-Arrays.sort(p);
-System.out.println("::Possible sequence of activities are ::");
-System.out.println("(" + p[j].start + "," + p[j].end +")"); 
-for(int i=1;i<n;i++)
-{
-	if(p[i].start >= p[j].end)
-	{
-		System.out.println("(" + p[i].start + "," + p[i].end + ")");
-		count++;
-		j = i;
-	}	
-	}
-
-return count;
-}
 }
