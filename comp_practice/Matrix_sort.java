29b29f9ab1b9989e24bed3395e582260308c86a7
Rajan-Pandey <pandeyrajan960@gmail.com>
2022-09-14 19:49:51 +0530

    Sort the matrix row-wise and column-wise

---
+++ comp_practice/Matrix_sort.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Matrix_sort
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter no of rows and columns : ");
+int m = sc.nextInt();
+int n = sc.nextInt();
+
+int[][] arr = new int[m][n];
+for(int i = 0; i<m ; i++)
+	for(int j =0; j<n; j++)
+		arr[i][j] = sc.nextInt();
+
+for(int i =0; i<m;i++)
+	Arrays.sort(arr[i]);
+
+for(int i =0; i<m; i++)
+	for(int j = i+1; j<n; j++)
+	{
+		int temp = arr[i][j];
+		arr[i][j] = arr[j][i];
+		arr[j][i] = temp;
+	}
+
+for(int i =0; i<m;i++)
+	Arrays.sort(arr[i]);
+
+for(int i =0; i<m; i++)
+	for(int j = i+1; j<n; j++)
+	{
+		int temp = arr[i][j];
+		arr[i][j] = arr[j][i];
+		arr[j][i] = temp;
+	}
+
+
+for(int i = 0; i<m ; i++)
+	{
+	for(int j =0; j<n; j++)
+		System.out.print(arr[i][j] + " ");
+	System.out.println();
+	}
+
+
+
+
+}
+}
