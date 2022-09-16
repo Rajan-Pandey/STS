6fe15d4ddffb17936922c62d1f7f7b008b97087d
Rajan-Pandey <pandeyrajan960@gmail.com>
2022-09-16 21:55:21 +0530

    All Common Subsequence

--- String_math/AllSubsequences.java
+++ String_math/AllSubsequences.java
@@ -4,13 +4,32 @@ public class AllSubsequences
 {
 
 static ArrayList<String> al = new ArrayList<String>();
+static ArrayList<String> al1 = new ArrayList<String>();
 public static void main(String[] args)
 {
+
+//double start = System.currentTimeMillis();
 Scanner sc = new Scanner(System.in);
 String str = sc.next();
+String str1 =sc.next();
 
 printSubsequence(str,"");
+printSubsequence1(str1,"");
+
+Collections.sort(al, (a,b) -> b.length() - a.length());
+Collections.sort(al1, (a,b) -> b.length() - a.length());
 System.out.println(al);
+System.out.println(al1);
+
+for(String s : al)
+{
+	if(al.contains(s) && al1.contains(s))	
+		System.out.println(s);
+}
+
+//double end = System.currentTimeMillis();
+
+//System.out.print((end-start)/1000);
 }
 
 
@@ -27,5 +46,19 @@ if(str.length() == 0)
         // because the concept of subsequence either
         // character will present or not	
 	printSubsequence(str.substring(1), ans);
+}
+static  void printSubsequence1(String str , String ans)
+{
+if(str.length() == 0)	
+	{
+			al1.add(ans);
+			return ;
+	}
+	 // We add adding 1st character in string
+	printSubsequence1(str.substring(1), ans+str.charAt(0));
+	// Not adding first character of the string
+        // because the concept of subsequence either
+        // character will present or not	
+	printSubsequence1(str.substring(1), ans);
 }
 }
